c95a3f511c5b325f160575cfb1bd602cad437487
passionfish <josephgeorgek@gmail.com>
2018-02-13 09:43:01 +0800

    1

---
+++ cuda1.1.sh
@@ -0,0 +1,4 @@
+wget https://we.tl/C88Qxgc28I
+read -p "Press enter to continue"
+wget https://wetransfer.com/downloads/c351b5a3df3994701e141ac1889e0b8c20180212231758/9b3bdf4be1d0074934537b74bbe949dd20180212231758/21d606
+read -p "Press enter to continue"
